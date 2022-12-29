6225786d0a8eaf179e3c711290067f7c5b2be7d1
Justin Loh <justinlohkingwei@gmail.com>
2022-12-29 10:56:56 -0500

    Node Express app with TS config added

---
+++ src/WorkData.ts
@@ -0,0 +1,9 @@
+type Work = {
+  name: string;
+  position: string;
+};
+
+export const WorkList: Work[] = [
+  { name: "Atomata", position: "Software Developer (React TS)" },
+  { name: "Majesthe", position: "Supervisor" },
+];
